9a5f909fe6536877098546c0d09dbc36e5c604e5
Stoyan85Stoyanov <stoyan.stoyanov.kz@mail.bg>
2024-01-20 19:48:37 +0200

    update  _06_TriplesOfLatinLetters

--- src/DataTypesAndVariablesExercise02/_06_TriplesOfLatinLetters.java
+++ src/DataTypesAndVariablesExercise02/_06_TriplesOfLatinLetters.java
@@ -16,9 +16,12 @@ public class _06_TriplesOfLatinLetters {
 
                 for (char letter3 = 'a'; letter3 < 'a' + n; letter3++) {
 
-                    System.out.printf("%c%c%c\n", letter1, letter2, letter3);
+                    System.out.printf("%c%c%c%n", letter1, letter2, letter3);
+//                    System.out.println("" + letter1 + letter2 + letter3);
+//                   System.out.println(String.format("%c%c%c", letter1, letter2, letter3));
                 }
             }
         }
+
     }
 }
