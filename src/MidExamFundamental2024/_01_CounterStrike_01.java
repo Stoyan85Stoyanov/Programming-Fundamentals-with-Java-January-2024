1721c3c9f1a36597e83dc6b3ef9cd0405bdffeba
Stoyan85Stoyanov <stoyan.stoyanov.kz@mail.bg>
2024-02-14 15:35:36 +0200

    MidExamFundamental2024

---
+++ src/MidExamFundamental2024/_01_CounterStrike_01.java
@@ -0,0 +1,36 @@
+package MidExamFundamental2024;
+
+import java.util.Scanner;
+
+public class _01_CounterStrike_01 {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+
+        int energy = Integer.parseInt(scanner.nextLine());
+        int wins = 0;
+
+        String input = scanner.nextLine();
+        while (!input.equals("End of battle")) {
+
+            int distance = Integer.parseInt(input);
+            if (distance <= energy) {
+                wins++;
+                energy -= distance;
+
+                if (wins % 3 == 0) {
+                    energy += wins;
+                }
+            } else {
+                System.out.printf("Not enough energy! Game ends with %d won battles and %d energy", wins, energy);
+                return;
+            }
+
+            input = scanner.nextLine();
+        }
+
+        System.out.printf("Won battles: %d. Energy left: %d", wins, energy);
+    }
+}
+
