1afe9869488576b14719a9f8d80d0d18af4a7355
Stoyan85Stoyanov <stoyan.stoyanov.kz@mail.bg>
2024-03-06 23:42:47 +0200

    TextProcessingLab08

---
+++ src/TextProcessingLab08/_05_DigitsLettersAndOther.java
@@ -0,0 +1,32 @@
+package TextProcessingLab08;
+
+import java.util.Scanner;
+
+public class _05_DigitsLettersAndOther {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String input = scanner.nextLine();
+
+        StringBuilder digits = new StringBuilder();
+        StringBuilder letters = new StringBuilder();
+        StringBuilder others = new StringBuilder();
+
+        for (int i = 0; i < input.length(); i++) {
+
+            char symbol = input.charAt(i);
+
+            if (Character.isDigit(symbol)) {
+                digits.append(symbol);
+
+            }else if (Character.isLetter(symbol)) {
+                letters.append(symbol);
+
+            }else{
+                others.append(symbol);
+            }
+        }
+        System.out.printf("%s%n%s%n%s%n", digits, letters, others);
+    }
+}
