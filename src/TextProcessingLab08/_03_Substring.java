c329d9dcab1abaa651597c424d6b6c7cdbeed6c3
Stoyan85Stoyanov <stoyan.stoyanov.kz@mail.bg>
2024-03-06 23:29:27 +0200

    TextProcessingLab08

---
+++ src/TextProcessingLab08/_03_Substring.java
@@ -0,0 +1,23 @@
+package TextProcessingLab08;
+
+import java.util.Scanner;
+
+public class _03_Substring {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+           String firstText = scanner.nextLine();
+           String secondText = scanner.nextLine();
+
+           int index = secondText.indexOf(firstText);
+
+           while (index != -1) {
+
+               secondText = secondText.replace(firstText, "");
+               index = secondText.indexOf(firstText);
+
+           }
+        System.out.println(secondText);
+    }
+}
