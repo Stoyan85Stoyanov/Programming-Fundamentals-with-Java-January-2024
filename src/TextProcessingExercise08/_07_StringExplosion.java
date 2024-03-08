c01355a96123bdf6dd07b50d21150d656136bbfa
Stoyan85Stoyanov <stoyan.stoyanov.kz@mail.bg>
2024-03-08 23:43:19 +0200

    TextProcessingExercise08

---
+++ src/TextProcessingExercise08/_07_StringExplosion.java
@@ -0,0 +1,32 @@
+package TextProcessingExercise08;
+
+import java.util.Scanner;
+
+public class _07_StringExplosion {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String input = scanner.nextLine();
+        StringBuilder textBuilder = new StringBuilder(input);
+
+        int totalStrength = 0;
+
+        for (int position = 0; position < textBuilder.length(); position++) {
+
+            char currentSymbol = textBuilder.charAt(position);
+
+            if (currentSymbol == '>') {
+
+                int explosionStrength = Integer.parseInt(String.valueOf(textBuilder.charAt(position + 1)));
+                totalStrength += explosionStrength;
+
+            }else if (totalStrength > 0) {
+                textBuilder.deleteCharAt(position);
+                totalStrength --;
+                position --;
+            }
+        }
+        System.out.println(textBuilder);
+    }
+}
