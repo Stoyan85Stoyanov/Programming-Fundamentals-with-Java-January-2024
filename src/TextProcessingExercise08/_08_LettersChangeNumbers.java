925513a8a664af77effa7c89158c7eca4b89f568
Stoyan85Stoyanov <stoyan.stoyanov.kz@mail.bg>
2024-03-09 15:31:30 +0200

    TextProcessingExercise08

---
+++ src/TextProcessingExercise08/_08_LettersChangeNumbers.java
@@ -0,0 +1,44 @@
+package TextProcessingExercise08;
+
+import java.util.Scanner;
+
+public class _08_LettersChangeNumbers {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String [] codes = scanner.nextLine().split("\\s+");
+
+        double sum = 0;
+
+        for (String code : codes) {
+             double result = calculateCodeResult(code);
+             sum += result;
+
+        }
+        System.out.printf("%.2f%n", sum);
+    }
+
+    private static double calculateCodeResult(String code) {
+
+        char letterBefore = code.charAt(0);
+        double number = Double.parseDouble(code.substring(1, code.length() - 1));
+        char letterAfter = code.charAt(code.length() - 1) ;
+
+        if (Character.isUpperCase(letterBefore)) {
+            number /= letterBefore - 64;
+
+        }else {
+            number *= letterBefore - 96;
+        }
+
+        if (Character.isUpperCase(letterAfter)) {
+            number -= letterAfter - 64;
+
+        }else {
+            number += letterAfter - 96;
+        }
+
+        return number;
+    }
+}
