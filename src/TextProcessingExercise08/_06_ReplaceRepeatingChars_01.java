6dc8bb70f4c7ad31993f0756599078c697d26075
Stoyan85Stoyanov <stoyan.stoyanov.kz@mail.bg>
2024-03-11 21:22:48 +0200

    TextProcessingExercise08

---
+++ src/TextProcessingExercise08/_06_ReplaceRepeatingChars_01.java
@@ -0,0 +1,22 @@
+package TextProcessingExercise08;
+
+import java.util.Scanner;
+
+public class _06_ReplaceRepeatingChars_01 {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String input = scanner.nextLine();
+        StringBuilder textBuilder = new StringBuilder(input);
+
+        for (int position = 0; position < textBuilder.length() - 1; position++) {
+
+            if (textBuilder.charAt(position) == textBuilder.charAt(position + 1)) {
+                textBuilder.deleteCharAt(position + 1);
+                position--;
+            }
+        }
+        System.out.println(textBuilder);
+    }
+}
