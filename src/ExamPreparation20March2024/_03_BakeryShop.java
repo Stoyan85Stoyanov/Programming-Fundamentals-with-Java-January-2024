0f4301332c9f76028a2e9b9aa38a440aef60b20d
Stoyan85Stoyanov <stoyan.stoyanov.kz@mail.bg>
2024-03-30 11:50:16 +0200

    ExamPreparation20March2024

--- src/ExamPreparation20March2024/_03_BakeryShop.java
+++ src/ExamPreparation20March2024/_03_BakeryShop.java
@@ -67,8 +67,8 @@ public class _03_BakeryShop {
                 int amountFood = inStock.getValue();
                 System.out.println(stock + ": " + amountFood);
             }
-            System.out.printf("All sold: %d goods", foofSum);
-            System.out.println();
+            System.out.printf("All sold: %d goods%n", foofSum);
+
         }
     }
 
