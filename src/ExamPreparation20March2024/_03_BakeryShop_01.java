4ee8dab518ad227493aa59c378f02ee15e9b9c93
Stoyan85Stoyanov <stoyan.stoyanov.kz@mail.bg>
2024-04-10 21:51:40 +0300

    ExamPreparation20March2024

---
+++ src/ExamPreparation20March2024/_03_BakeryShop_01.java
@@ -0,0 +1,73 @@
+package ExamPreparation20March2024;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+public class _03_BakeryShop_01 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        Map<String, Integer> commandMap = new LinkedHashMap<>();
+
+        String commandInput = scanner.nextLine();
+        int foofSum = 0;
+
+        while (!commandInput.equals("Complete")) {
+
+            String[] commandParts = commandInput.split("\\s+");
+            String command = commandParts[0];
+            int quantity = Integer.parseInt(commandParts[1]);
+            String food = commandParts[2];
+            int currentQuantity ;
+
+            switch (command) {
+
+                case "Receive":
+                    if (quantity >= 0) {
+                        if (!commandMap.containsKey(food)) {
+                            commandMap.put(food, quantity);
+                        } else {
+                            currentQuantity = commandMap.get(food);
+                            commandMap.put(food, currentQuantity + quantity);
+                        }
+                    }
+                    break;
+
+                case "Sell":
+                    if (!commandMap.containsKey(food)) {
+                        System.out.printf("You do not have any %s.%n", food);
+
+                    } else {
+                        currentQuantity = commandMap.get(food);
+
+                        if (quantity > currentQuantity) {
+
+                            foofSum = foofSum + currentQuantity;
+                            System.out.printf("There aren't enough %s. You sold the last %d of them.%n", food, currentQuantity);
+                            commandMap.remove(food);
+
+                        } else {
+                            foofSum = foofSum + quantity;
+                            System.out.printf("You sold %d %s.%n", quantity, food);
+
+                            commandMap.put(food, currentQuantity - quantity);
+
+                            if (currentQuantity - quantity <= 0) {
+                                commandMap.remove(food);
+                            }
+                        }
+                    }
+                    break;
+            }
+            commandInput = scanner.nextLine();
+        }
+            for (Map.Entry<String, Integer> inStock : commandMap.entrySet()) {
+                String stock = inStock.getKey();
+                int amountFood = inStock.getValue();
+                System.out.println(stock + ": " + amountFood);
+            }
+            System.out.printf("All sold: %d goods%n", foofSum);
+        }
+    }
+
