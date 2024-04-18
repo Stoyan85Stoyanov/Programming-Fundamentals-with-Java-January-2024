a0ddd25c05eed61f956a50d9b6286f349772520d
Stoyan85Stoyanov <stoyan.stoyanov.kz@mail.bg>
2024-04-18 21:22:50 +0300

    FinalExamPreparation20March2024

--- src/FinalExamPreparation20March2024/_01_ActivationKeys.java
+++ src/FinalExamPreparation20March2024/_01_ActivationKeys.java
@@ -61,6 +61,7 @@ public class _01_ActivationKeys {
             }
             command = scanner.nextLine();
         }
-        System.out.printf("Your activation key is: %s%n", activationKey);
+        System.out.printf("Your activation key is: %s", activationKey);
+        System.out.println();
     }
 }
