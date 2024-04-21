5bee7c14a77f6d5fa8aa6c66734d9652342578fa
Stoyan85Stoyanov <stoyan.stoyanov.kz@mail.bg>
2024-04-21 21:34:44 +0300

    FinalExamPreparation20March2024

--- src/FinalExamPreparation20March2024/_02_DestinationMapper.java
+++ src/FinalExamPreparation20March2024/_02_DestinationMapper.java
@@ -29,6 +29,5 @@ public class _02_DestinationMapper {
             travelPoints += destination.length();
         }
         System.out.printf("Travel Points: %d", travelPoints);
-        System.out.println();
     }
 }
