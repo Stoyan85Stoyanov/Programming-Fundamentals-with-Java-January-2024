f648a066639a116f5f541fd21fc4285dabef418c
Stoyan85Stoyanov <stoyan.stoyanov.kz@mail.bg>
2024-04-21 21:33:42 +0300

    FinalExamPreparation20March2024

--- src/FinalExamPreparation20March2024/_02_DestinationMapper.java
+++ src/FinalExamPreparation20March2024/_02_DestinationMapper.java
@@ -22,7 +22,6 @@ public class _02_DestinationMapper {
         while (matcher.find()) {
             validDestinations.add(matcher.group("destination"));
         }
-
         System.out.println("Destinations: " + String.join(", ", validDestinations));
 
         int travelPoints = 0;
