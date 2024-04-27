23149895cbb69dd0a7454ed3ed319a66a88e144d
Stoyan85Stoyanov <stoyan.stoyanov.kz@mail.bg>
2024-04-27 23:48:44 +0300

    ExamPreparation20March2024

--- src/ExamPreparation20March2024/_01_Registration.java
+++ src/ExamPreparation20March2024/_01_Registration.java
@@ -24,8 +24,7 @@ public class _01_Registration {
                     } else if (upperOrLower.equals("Lower")) {
                         userName = userName.toLowerCase();
                     }
-                    System.out.print(userName);
-                    System.out.println();
+                    System.out.println(userName);
 
                     break;
 
